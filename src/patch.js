b5c42efc3041def01c6a3e7380fd4d55daa0884e
Julien Fontanet <julien.fontanet@isonoe.net>
2016-08-17 12:43:53 +0200

    feat(patch): apply changes to an object

---
+++ src/patch.js
@@ -0,0 +1,52 @@
+import forEach from 'lodash/forEach'
+import isArray from 'lodash/isArray'
+import isPlainObject from 'lodash/isPlainObject'
+import { createPredicate } from 'value-matcher'
+
+const applyPatch = (value, patch) => {
+  if (isPlainObject(patch)) {
+    if (isArray(value)) {
+      const toRemove = createPredicate(patch['-'])
+      const tmp = []
+      forEach(value, (v, i) => {
+        if (i in patch) {
+          const p = patch[i]
+          if (p === null) {
+            return
+          }
+          tmp.push(applyPatch(v, p))
+        } else if (!toRemove(v)) {
+          tmp.push(v)
+        }
+      })
+      const toAdd = patch['+']
+      if (toAdd) {
+        tmp.push.apply(tmp, toAdd)
+      }
+      return tmp
+    }
+
+    if (isPlainObject(value)) {
+      value = { ...value }
+      forEach(patch, (v, k) => {
+        if (v === null) {
+          delete value[k]
+        } else {
+          value[k] = applyPatch(value[k], v)
+        }
+      })
+      return value
+    }
+
+    value = {}
+    forEach(patch, (v, k) => {
+      if (v !== null) {
+        value[k] = applyPatch(null, v)
+      }
+    })
+    return patch
+  }
+
+  return patch
+}
+export { applyPatch as default }
